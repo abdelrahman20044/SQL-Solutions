79fc95e63b574ef89a87b46c61d4bd9b51a35da6
AbdelrahmanElmansy <121033553+abdelrahman20044@users.noreply.github.com>
2025-09-22 14:46:20 +0300

    Update 8-Customer Who Visited but Did Not Make Any Transactions.sql

--- SQL 50 leetcode/8-Customer Who Visited but Did Not Make Any Transactions.sql
+++ SQL 50 leetcode/8-Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,5 +1,5 @@
-with joined as (
+with NewTable as (
 select visit_id , customer_id 
 from Visits
 WHERE visit_id NOT IN (SELECT visit_id FROM Transactions )
-) select customer_id , count(visit_id ) as count_no_trans from joined group by customer_id 
+) select customer_id , count(visit_id ) as count_no_trans from NewTable group by customer_id 
