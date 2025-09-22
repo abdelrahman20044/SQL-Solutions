c87e30fd82b3454b8c9a37a53b63ac32c24e8120
AbdelrahmanElmansy <121033553+abdelrahman20044@users.noreply.github.com>
2025-09-22 15:01:21 +0300

    Update 8-Customer Who Visited but Did Not Make Any Transactions.sql

--- SQL 50 leetcode/8-Customer Who Visited but Did Not Make Any Transactions.sql
+++ SQL 50 leetcode/8-Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,5 +1,19 @@
+-- my sol
 with NewTable as (
 select visit_id , customer_id 
 from Visits
 WHERE visit_id NOT IN (SELECT visit_id FROM Transactions )
 ) select customer_id , count(visit_id ) as count_no_trans from NewTable group by customer_id 
+
+
+-- leetcode sol 
+SELECT 
+  customer_id, 
+  COUNT(*) AS count_no_trans 
+FROM 
+  Visits AS v 
+  LEFT JOIN Transactions AS t ON v.visit_id = t.visit_id 
+WHERE 
+  t.visit_id IS NULL 
+GROUP BY 
+  customer_id
